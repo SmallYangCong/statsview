763d75c093f0a3456c6cccc51fb3fc3d0eee7318
chaoyuepan <chaoyuepan@baidu.com>
2024-04-03 12:28:51 +0800

    add app cpu usage

--- go.mod
+++ go.mod
@@ -5,4 +5,11 @@ go 1.20
 require (
 	github.com/go-echarts/go-echarts/v2 v2.2.3
 	github.com/rs/cors v1.7.0
+	github.com/shirou/gopsutil/v3 v3.20.10
+)
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	golang.org/x/sys v0.0.0-20201024232916-9f70ab9862d5 // indirect
 )
--- viewer/gccpufraction.go
+++ viewer/gccpufraction.go
@@ -3,9 +3,12 @@ package viewer
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/shirou/gopsutil/v3/process"
 )
 
 const (
@@ -17,19 +20,23 @@ const (
 type GCCPUFractionViewer struct {
 	smgr  *StatsMgr
 	graph *charts.Line
+	p     *process.Process
 }
 
 // NewGCCPUFractionViewer returns the GCCPUFractionViewer instance
 // Series: Fraction
 func NewGCCPUFractionViewer() Viewer {
+	p, _ := process.NewProcess(int32(os.Getpid()))
+
 	graph := NewBasicView(VGCCPUFraction)
 	graph.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{Title: "GC CPUFraction"}),
+		charts.WithTitleOpts(opts.Title{Title: "CPUFraction"}),
 		charts.WithYAxisOpts(opts.YAxis{Name: "Percent", AxisLabel: &opts.AxisLabel{Show: true, Formatter: "{value} %", Rotate: 35}}),
 	)
-	graph.AddSeries("Fraction", []opts.LineData{})
+	graph.AddSeries("GC CPUFraction", []opts.LineData{})
+	graph.AddSeries("App CPUFraction", []opts.LineData{})
 
-	return &GCCPUFractionViewer{graph: graph}
+	return &GCCPUFractionViewer{graph: graph, p: p}
 }
 
 func (vr *GCCPUFractionViewer) SetStatsMgr(smgr *StatsMgr) {
@@ -48,10 +55,22 @@ func (vr *GCCPUFractionViewer) Serve(w http.ResponseWriter, _ *http.Request) {
 	vr.smgr.Tick()
 
 	metrics := Metrics{
-		Values: []float64{FixedPrecision(memstats.Stats.GCCPUFraction, 6)},
-		Time:   memstats.T,
+		Values: []float64{
+			FixedPrecision(memstats.Stats.GCCPUFraction, 6),
+			FixedPrecision(vr.getAppCPUFraction(), 6),
+		},
+		Time: memstats.T,
 	}
 
 	bs, _ := json.Marshal(metrics)
 	w.Write(bs)
+}
+
+func (vr *GCCPUFractionViewer) getAppCPUFraction() float64 {
+	p := vr.p
+	if p == nil {
+		return 0.0
+	}
+	percent, _ := p.Percent(time.Second)
+	return percent
 }
